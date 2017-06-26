04406fc4626e0203d3f9ac85100280e984a12f50
J.Stone <jameson@stone.tc>
2017-06-26 18:29:18 -0400

    meeeehhhhh

--- test/lib/hello-user-test.js
+++ test/lib/hello-user-test.js
@@ -5,19 +5,19 @@ module.exports = {
 	BobTest : function() {
 		var name = 'Bob';
 		var result = subject.greeting( name );
-		assert.equal( result, 'Hello, Bob!' );
+		assert.equal( result, 'Hello, Bob.' );
 	},
 	MyFriendUndefinedTest : function() {
 		var result = subject.greeting();
-		assert.equal( result, 'Hello, my friend!' );
+		assert.equal( result, 'Hello, my friend.' );
 	},
 	MyFriendEmptyStringTest : function() {
 		var result = subject.greeting( '' );
-		assert.equal( result, 'Hello, my friend!' );
+		assert.equal( result, 'Hello, my friend.' );
 	},
 	MyFriendEmptyStringWithSpaceTest : function() {
 		var result = subject.greeting( ' ' );
-		assert.equal( result, 'Hello, my friend!' );
+		assert.equal( result, 'Hello, my friend.' );
 	},
 	JerryUppercaseTest : function() {
 		var result = subject.greeting( 'JERRY' );
@@ -26,16 +26,21 @@ module.exports = {
 	ListGreetingOfTwoTest : function() {
 		var listOfNames = [ 'Jill', 'Jane' ];
 		var result = subject.greeting( listOfNames );
-		assert.equal( result, 'Hello, Jill and Jane' );
+		assert.equal( result, 'Hello, Jill and Jane.' );
 	},
 	ListGreetingOfThreeTest : function() {
 		var listOfNames = [ 'Amy', 'Brian', 'Charlotte' ];
 		var result = subject.greeting( listOfNames );
-		assert.equal( result, 'Hello, Amy, Brian, and Charlotte' );
+		assert.equal( result, 'Hello, Amy, Brian, and Charlotte.' );
 	},
 	ListGreetingWithCAPSTest : function() {
 		var listOfNames = [ 'Amy', 'BRIAN', 'Charlotte' ];
 		var result = subject.greeting( listOfNames );
 		assert.equal( result, 'Hello, Amy and Charlotte. AND HELLO BRIAN!' );
+	},
+	ListGreetingWithIntentionalQuotes : function() {
+		var listOfNames = [ 'Bob', "\"Charlie, Dianne\"" ];
+		var result = subject.greeting( listOfNames );
+		assert.equal( result, 'Hello, Bob and Charlie, Dianne.' );
 	}
 };
